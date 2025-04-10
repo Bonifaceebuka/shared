c29fc2afca07e611311cf583cdd52e1eadeed201
Boniface <boniface.ebuka@gmail.com>
2025-04-10 10:39:56 +0100

    Setup

---
+++ src/constants/queues.ts
@@ -0,0 +1,9 @@
+export const QUEUE_NAMES = {
+    EMAIL_VERIFICATION: {
+        NAME: 'email_verification_queue',
+        SUBJECT: "Account Activation"
+    },
+    NEW_REGISTRATION: {
+        NAME: 'create_new_user'
+    }
+}
